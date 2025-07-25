4a348d16028865503003799d5b52c77dc29b8984
Gundeboina Sanjay <sanjaygundeboina9@gmail.com>
2025-07-25 23:01:59 +0530

    js_task

--- timer/script.js
+++ timer/script.js
@@ -1,70 +1,73 @@
-let ten=document.getElementById("ten")
-let twenty=document.getElementById("twenty")
-let thirty=document.getElementById("thirty")
-let timer=document.getElementById("timer")
-let min=0;
-let sec=0;
-ten.onclick=()=>{
-   min=9;
-   sec=59;
-    let time=setInterval(()=>{
-     sec--;
-     if(sec==0){
-        min--;
-        sec=59;
-     }   
-   
-    timer.textContent=(min<10?"0"+min:min)+":"+(sec<10?"0"+sec:sec);
-    console.log(timer)
+let ten = document.getElementById("ten")
+let twenty = document.getElementById("twenty")
+let thirty = document.getElementById("thirty")
+let timer = document.getElementById("timer")
+let min = 0;
+let sec = 0;
+let time; // global variable to control setInterval
 
-    if(min<0){
-       sec=0
-       min=0
-       timer.textContent=min+"0"+":"+sec+"0"
-       clearInterval(time)
-    }
-   },100)
+ten.onclick = () => {
+    clearInterval(time); // stop any running timer
+    min = 9;
+    sec = 60;
+    time = setInterval(() => {
+        sec--;
+        if (sec == 0) {
+            min--;
+            sec = 59;
+        }
+
+        timer.textContent = (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
+        console.log(timer)
+
+        if (min < 0) {
+            sec = 0
+            min = 0
+            timer.textContent = min + "0" + ":" + sec + "0"
+            clearInterval(time)
+        }
+    }, 1000)
 }
-twenty.onclick=()=>{
-   min=19;
-   sec=59;
-   let time=setInterval(()=>{
-     sec--;
-     if(sec==0){
-        min--;
-        sec=59
-     }
-   timer.textContent=(min<10?"0"+min:min)+":"+(sec<10?"0"+sec:sec);
-   console.log(timer)
 
-   if(min<0){
-    sec=0
-    min=0
-    timer.textContent=min+"0"+":"+sec+"0"
-    clearInterval(time)
-   }
-   },10)
+twenty.onclick = () => {
+    clearInterval(time);
+    min = 19;
+    sec = 60;
+    time = setInterval(() => {
+        sec--;
+        if (sec == 0) {
+            min--;
+            sec = 59
+        }
+        timer.textContent = (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
+        console.log(timer)
 
-   }
+        if (min < 0) {
+            sec = 0
+            min = 0
+            timer.textContent = min + "0" + ":" + sec + "0"
+            clearInterval(time)
+        }
+    }, 1000)
+}
 
-thirty.onclick=()=>{
-   min=29;
-   sec=59;
-   let time=setInterval(()=>{
-     sec--;
-     if(sec==0){
-        min--;
-        sec=59
-     }
-   timer.textContent=(min<10?"0"+min:min)+":"+(sec<10?"0"+sec:sec);
-   console.log(timer)
-   if(min<0){
-    sec=0
-    min=0
-    timer.textContent=min+"0"+":"+sec+"0"
-    clearInterval(time)
-   }
-   },10)
-
-   }
- 
+thirty.onclick = () => {
+    clearInterval(time);
+    min = 29;
+    sec = 60;
+    time = setInterval(() => {
+        sec--;
+        if (sec == 0) {
+            min--;
+            sec = 59
+        }
+        timer.textContent = (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
+        console.log(timer)
+        if (min < 0) {
+            sec = 0
+            min = 0
+            timer.textContent = min + "0" + ":" + sec + "0"
+            clearInterval(time)
+        }
+    }, 1000)
+}
